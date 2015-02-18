dd8bc6396bc37a2366dab90e09966837f7c4825d
FRC5113 <frc5113@gmail.com>
2015-02-18 15:12:17 -0500

    Basic Orbit Code
    Not yet done

--- FRC5113_2015/src/org/usfirst/frc/team5113/auton/OrbitToteGoal.java
+++ FRC5113_2015/src/org/usfirst/frc/team5113/auton/OrbitToteGoal.java
@@ -1,6 +1,20 @@
 package org.usfirst.frc.team5113.auton;
 
+import org.usfirst.frc.team5113.drive.MotorManager;
+
 public class OrbitToteGoal
 {
-
+	float shortLength, distance, currLength;
+	final float TOL = 15;
+	
+	public void update(MotorManager dr)
+	{
+		if(distance > 2)
+			shortLength = (float)(300 * Math.pow(distance, -1.09));
+		else
+			shortLength = (float)(310 * Math.pow(distance, -1.2));
+		
+		while((currLength - shortLength) > TOL)
+			dr.mecanumDrive(0.8f, 90, 0.2f);
+	}
 }
