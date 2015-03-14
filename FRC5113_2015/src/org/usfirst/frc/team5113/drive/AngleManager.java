52ee02a186ec702ccbe9abf51a78c8b69f96cbf9
FRC5113 <frc5113@gmail.com>
2015-03-14 10:42:49 -0400

    AngleManager Updated with gyro
    what the title said.

--- FRC5113_2015/src/org/usfirst/frc/team5113/drive/AngleManager.java
+++ FRC5113_2015/src/org/usfirst/frc/team5113/drive/AngleManager.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team5113.drive;
 
 import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 
 public class AngleManager 
@@ -9,9 +11,13 @@ public class AngleManager
 	ADXL345_I2C accel;//Ari confirmed that we do infact use I2C
 	ADXL345_I2C.AllAxes accelerations;
 	
+	//Gyro
+	Gyro gyro;
+	
 	public void init()
 	{
-		accel = new ADXL345_I2C(null, Range.k4G);
+		accel = new ADXL345_I2C(Port.kMXP, Range.k4G);
+		gyro =  new Gyro(1);
 	}
 	
 	public double[] accelVals()
@@ -22,6 +28,10 @@ public class AngleManager
 		vals[2] = accel.getAcceleration(ADXL345_I2C.Axes.kZ);
 		
 		return vals;//x, y, z
-		
+	}
+	
+	public double currAngle()
+	{
+		return gyro.getAngle();
 	}
 }
